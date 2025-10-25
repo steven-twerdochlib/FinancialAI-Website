6a04147b72dfa3a10131df3df583633be65c9f35
Steven Twerdochlib <steven.twerdochlib@gmail.com>
2025-10-25 20:53:36 +0100

    Changed url as previous was taken

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/' // <- crucial for GitHub Pages
+  base: '/FinancialAI-Website/' // <- crucial for GitHub Pages
 })
